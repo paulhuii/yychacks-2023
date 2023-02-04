18d7458953d72e5c0abae0fb4d783254759a98bf
rosemuhammed <rose.muhammed0159@gmail.com>
2023-02-04 12:57:41 -0700

    created outline of login & create account pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,29 @@
 const Login = () => {
     return (
         <div>
-            Login
+            <h1>Log in</h1>
+
+            <form>
+                <p>
+                    <label for="email">
+                        <span>Email: </span>
+                        <strong><span aria-label="required">*</span></strong>
+                    </label>
+                    <input type="email" id="useremail" name="useremail"/>
+                </p>
+
+                <p>
+                    <label for="pwd">
+                        <span> Password: </span>
+                        <strong><span aria-label="required">*</span></strong>
+                    </label>
+                    <input type="password" id="pwd" name="password"/>
+                </p>
+
+                <div>
+                    <button type="submit">Create Account</button>
+                </div>
+            </form>
         </div>
     )
 }
