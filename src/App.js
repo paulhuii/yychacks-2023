78424c0b3499d27508aa3a3c2afb4aa8441c8767
Paul Hui <pcyhui@ucalgary.ca>
2023-02-04 11:52:13 -0700

    Added spinning wheel for prizes. 4 files. SW, wheel, and accompanying css files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from './pages/Home'
 import Calendar from './pages/Calendar'
+import SW from './pages/SW'
 import Login from './pages/Login'
 import CreateAccount from './pages/CreateAccount'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
@@ -11,10 +12,9 @@ const App = () => {
             <Route path={"/login"} element={<Login/>}/>
             <Route path={"/CreateAccount"} element={<CreateAccount/>}/>
             <Route path={"/Calendar"} element={<Calendar/>}/>
-
+            <Route path={"/SW"} element={<SW/>}/>
         </Routes>
       </BrowserRouter>
   );
 }
-
 export default App;
