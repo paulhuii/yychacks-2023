7cd92bdd07db4105383298eac4497d3e9d664fc8
Deborah Odoh <deborah.odo@edu.sait.ca>
2023-02-04 16:55:35 -0700

    Snowboard Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home'
+import Winsport from './pages/Winsport'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} element={<Home/>}/>
+        <Route path={"/"} element={<Home/>}/>
+        <Route path={"/Winsport"} element={<Winsport/>}/>
         </Routes>
       </BrowserRouter>
   );
