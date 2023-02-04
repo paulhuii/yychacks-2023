0eced6b99553c1c37d18b8dd0d4860659ab8b133
evan-losier <etlosier@gmail.com>
2023-02-04 13:36:56 -0700

    working map with pins, zoom, and center

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/Home'
+import MapPage from './pages/map'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<Home/>}/>
+          <Route path={"/map"} element={<MapPage/>}/>
         </Routes>
       </BrowserRouter>
   );
