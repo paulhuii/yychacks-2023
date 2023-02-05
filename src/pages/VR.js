7cfdd48a4b8082d54f1594777abbb6433b79dbb1
Deborah Odoh <deborah.odo@edu.sait.ca>
2023-02-04 19:15:07 -0700

    Last push

--- src/pages/VR.js
+++ src/pages/VR.js
@@ -1,4 +1,4 @@
-<div >
+<div>
                         <video height="auto" controls="controls" preload="none" onclick="this.play()">
                         <source type="video/mp4" src="vr_video.mp4"></source>
                         </video>
